997ebc80f12cf84d4fdf9803f210c770359f86bc
[dyad] <martinpensa1@gmail.com>
2025-10-03 15:21:55 +0200

    Reverted all changes back to version b68c321603a9a8c611de8fea339cd992992bda7c

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-preset-mantine': {},
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
